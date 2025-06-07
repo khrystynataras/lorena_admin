8fda5c6cbba980ddb12380c1cc50cc28f041359a
khrystynataras <kristina231004@gmail.com>
2025-06-07 23:31:14 +0300

    add console log for order details in OrderDetails component

--- app/(dashboard)/orders/[orderId]/page.tsx
+++ app/(dashboard)/orders/[orderId]/page.tsx
@@ -4,6 +4,8 @@ import { columns } from "@/components/orderItems/OrderItemsColums"
 const OrderDetails = async ({ params }: { params: { orderId: string } }) => {
   const res = await fetch(`${process.env.ADMIN_DASHBOARD_URL}/api/orders/${params.orderId}`)
   const { orderDetails, customer } = await res.json()
+  
+  console.log("Order Details:", orderDetails)
 
   const { street, city, state, postalCode, country } = orderDetails.shippingAddress
 
